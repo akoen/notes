8eb65624a280a0e54c68cd9c210f22c3dff09fa8
akoen <arw.koen@gmail.com>
2020-05-17 21:09:05 -0700

    2020-05-17

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,5 @@
 (("org/"
-  . ((org-mode . ((eval . (org-hugo-auto-export-mode))
-                  (eval . (olivetti-mode 1))
+  . ((org-mode . ((eval . (olivetti-mode 1))
                   (eval . (flyspell-mode 1))
                   (eval . (set-input-method 'TeX))
                   (eval . (display-line-numbers-mode -1)))))))
