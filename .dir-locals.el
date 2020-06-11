470f8b0743d70056aeb01ebbae7316509362631b
akoen <arw.koen@gmail.com>
2020-06-11 15:28:24 -0700

    2020-06-11

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
 (("org/"
   . ((org-mode . ((eval . (olivetti-mode 1))
                   (eval . (flyspell-mode 1))
+                  (eval . (variable-pitch-mode 1))
                   (eval . (set-input-method 'TeX))
                   (eval . (display-line-numbers-mode -1)))))))
