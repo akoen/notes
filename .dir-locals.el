3de905db2933caeb72b85dd77f9b5dbcd0a6e178
akoen <arw.koen@gmail.com>
2020-06-22 22:00:52 -0700

    2020-06-22

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
 (("org/"
   . ((org-mode . ((eval . (olivetti-mode 1))
                   (eval . (flyspell-mode 1))
+                  (eval . (org-roam-buffer-activate))
                   (eval . (variable-pitch-mode 1))
                   (eval . (set-input-method 'TeX))
                   (eval . (display-line-numbers-mode -1)))))))
